1c8ab6c61f96e9067622b4937eadc5449bfa9a83
Airita Shuksto <ashuksto@gmail.com>
2020-12-29 11:02:16 +0000

    1darbs

--- serveris.py
+++ serveris.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask,jsonify
+import dati
+
 
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII']=False
 
 @app.route('/')
 def index():
     return "Labrīt!"
-
+@app.route('/api/v1/vielas')
+def vielas():
+    return jsonify(dati.vielas)
+@app.route('/api/v1/viela/<vielasID>')
+def viela(vielasID):
+  viela=f"Viela ar doto id {vielasID} nav atrasta"
+  for v in dati.vielas:
+    if str(v['id'])==vielasID:
+      viela=v
+  return jsonify(viela)
+@app.route('/api/v1/inventars')
+def inventars():
+    return jsonify(dati.inventars) 
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
